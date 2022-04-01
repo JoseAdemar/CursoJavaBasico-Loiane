55f2ab6ca292014b324e0475530bff2e098c236c
jose_ademar <joseademarti@gmail.com>
2022-04-01 12:13:18 -0300

    adicionada as aulas iniciasi do curso

---
+++ Curso Loiane/aula07/Erro01.java
@@ -0,0 +1,5 @@
+public class Erro01{
+	public static void main(String[] args){
+		System.out.println("Você digitou: " + args[0]);
+	}
+}
---
+++ Curso Loiane/aula07/Erro02.java
@@ -0,0 +1,6 @@
+public class Erro02{
+	public static void main(String[] args){
+		int cont = 10;
+		System.out.println("Voce digitou " + cont);
+	}
+}
