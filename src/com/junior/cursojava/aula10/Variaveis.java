c698c825a79f139da1f834b7ade7c76bae5dbe95
jose_ademar <joseademarti@gmail.com>
2022-04-01 12:11:49 -0300

    Estudando variaveis

---
+++ src/com/junior/cursojava/aula10/Variaveis.java
@@ -0,0 +1,12 @@
+package com.junior.cursojava.aula10;
+
+public class Variaveis {
+	public static void main(String[] args) {
+		int idade = 31;
+		String nome = "Junior";
+		String nomeDoMeuCachorro = "Rook";
+
+		System.out.println(
+				"Meu nome é: " + nome + " Eu tenho: " + idade + " anos" + " Meu cachorro chama: " + nomeDoMeuCachorro);
+	}
+}
