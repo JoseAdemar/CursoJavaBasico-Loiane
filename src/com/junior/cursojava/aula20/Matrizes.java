faa0073d870abce84410a8ce6c0ed2f186abccfe
JoseJunior <joseademarti@gmail.com>
2022-07-05 21:38:51 -0300

    Realizado o exercicio 01 da aula 20 matrizes

---
+++ src/com/junior/cursojava/aula20/Matrizes.java
@@ -0,0 +1,25 @@
+package com.junior.cursojava.aula20;
+
+public class Matrizes {
+	public static void main(String[] args) {
+
+		double[][] notas = new double[30][4];
+		
+		notas[0][0] = 10;
+		notas[0][1] = 7;
+		notas[0][2] = 9;
+		notas[0][3] = 9.5;
+		
+		notas[1][0] = 10;
+		notas[1][1] = 7;
+		notas[1][2] = 9;
+		notas[1][3] = 9.5;
+		
+		notas[2][0] = 10;
+		notas[2][1] = 7;
+		notas[2][2] = 9;
+		notas[2][3] = 9.5;
+		
+	
+	}
+}
