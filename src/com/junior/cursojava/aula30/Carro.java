1cdf0d4133a954fb9a7c99e75af2e44e8ec079e7
JoseJunior <joseademarti@gmail.com>
2022-08-03 22:02:31 -0300

    Finalizado o modulo de modificadores de acesso

---
+++ src/com/junior/cursojava/aula30/Carro.java
@@ -0,0 +1,33 @@
+package com.junior.cursojava.aula30;
+
+public class Carro {
+
+	String marca;
+	String modelo;
+	int numPassageiros;
+	double capCombustivel;
+	double consumoCombustivel;
+
+	public Carro(String marca, String modelo, int numPassageiros, double capCombustivel, double consumoCombustivel) {
+		this.marca = marca;
+		this.modelo = modelo;
+		this.numPassageiros = numPassageiros;
+		this.capCombustivel = capCombustivel;
+		this.consumoCombustivel = consumoCombustivel;
+
+	}
+
+	public Double exibirAutonomia2() {
+
+		return this.capCombustivel * this.consumoCombustivel;
+
+	}
+
+	public double calcularCombustivel(double km) {
+
+		double qtdCombustivel = (km / consumoCombustivel);
+
+		return qtdCombustivel;
+	}
+
+}
