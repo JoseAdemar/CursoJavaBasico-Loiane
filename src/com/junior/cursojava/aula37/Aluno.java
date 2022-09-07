7b43f149df993b25b5883b2187c9346da333e442
JoseJunior <joseademarti@gmail.com>
2022-09-06 21:58:31 -0300

    Estudando orientação a objetos

---
+++ src/com/junior/cursojava/aula37/Aluno.java
@@ -0,0 +1,47 @@
+package com.junior.cursojava.aula37;
+
+import java.util.Arrays;
+
+public class Aluno extends Pessoa {
+
+	private String curso;
+	private double[] notas;
+
+	public double calcularMedia() {
+		return 0;
+	}
+
+	public boolean aprovado() {
+		return true;
+	}
+
+	public String getCurso() {
+		return curso;
+	}
+
+	public void setCurso(String curso) {
+		this.curso = curso;
+	}
+
+	public double[] getNotas() {
+		return notas;
+	}
+
+	public void setNotas(double[] notas) {
+		this.notas = notas;
+	}
+
+	@Override
+	public String toString() {
+		return "curso=" + curso + ", \n notas=" + Arrays.toString(notas) + ",\n getNome()=" + getNome()
+				+ ",\n getEndereco()=" + getEndereco() + ",\n getTelefone()=" + getTelefone() + ",\n getCpf()=" + getCpf()
+				+ "]";
+	}
+
+	
+
+	
+
+	
+
+}
---
+++ src/com/junior/cursojava/aula37/Professor.java
@@ -0,0 +1,37 @@
+package com.junior.cursojava.aula37;
+
+public class Professor extends Pessoa {
+
+	private double salario;
+	private String nomeCurso;
+	
+	
+	public double calcularSalarioLiquido() {
+		return 0;
+	}
+
+
+	public double getSalario() {
+		return salario;
+	}
+
+
+	public void setSalario(double salario) {
+		this.salario = salario;
+	}
+
+
+	public String getNomeCurso() {
+		return nomeCurso;
+	}
+
+
+	public void setNomeCurso(String nomeCurso) {
+		this.nomeCurso = nomeCurso;
+	}
+
+
+	
+	
+
+}
